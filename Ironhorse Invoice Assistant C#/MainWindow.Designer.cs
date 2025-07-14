6353580e1609e17b1ab6e67c3dc6b43fffc5a3b0
GameWizzard2 <98629019+GameWizzard2@users.noreply.github.com>
2025-07-13 22:06:03 -0400

    Added destination textbox, and selection button. Reformtted some code, README.md

--- Ironhorse Invoice Assistant C#/MainWindow.Designer.cs
+++ Ironhorse Invoice Assistant C#/MainWindow.Designer.cs
@@ -29,13 +29,17 @@
         private void InitializeComponent()
         {
             SelectFolder = new Button();
-            test = new Button();
-            testOutput = new RichTextBox();
+            buttonResizeImages = new Button();
+            textBoxSource = new RichTextBox();
+            labelResizeImage = new Label();
+            labelOutputLocation = new Label();
+            textBoxDestination = new RichTextBox();
+            buttonSelectFolderDistination = new Button();
             SuspendLayout();
             // 
             // SelectFolder
             // 
-            SelectFolder.Location = new Point(343, 251);
+            SelectFolder.Location = new Point(115, 306);
             SelectFolder.Name = "SelectFolder";
             SelectFolder.Size = new Size(134, 51);
             SelectFolder.TabIndex = 0;
@@ -43,43 +47,88 @@
             SelectFolder.UseVisualStyleBackColor = true;
             SelectFolder.Click += SelectFolder_Click;
             // 
-            // test
+            // buttonResizeImages
             // 
-            test.Location = new Point(454, 116);
-            test.Name = "test";
-            test.Size = new Size(112, 34);
-            test.TabIndex = 1;
-            test.Text = "button1";
-            test.UseVisualStyleBackColor = true;
-            test.Click += test_Click;
+            buttonResizeImages.Location = new Point(336, 354);
+            buttonResizeImages.Name = "buttonResizeImages";
+            buttonResizeImages.Size = new Size(140, 51);
+            buttonResizeImages.TabIndex = 1;
+            buttonResizeImages.Text = "Resize Images";
+            buttonResizeImages.UseVisualStyleBackColor = true;
+            buttonResizeImages.Click += test_Click;
             // 
-            // testOutput
+            // textBoxSource
             // 
-            testOutput.Location = new Point(164, 48);
-            testOutput.Name = "testOutput";
-            testOutput.Size = new Size(252, 178);
-            testOutput.TabIndex = 2;
-            testOutput.Text = "";
-            testOutput.TextChanged += testOutput_TextChanged;
+            textBoxSource.Location = new Point(64, 122);
+            textBoxSource.Name = "textBoxSource";
+            textBoxSource.Size = new Size(252, 178);
+            textBoxSource.TabIndex = 2;
+            textBoxSource.Text = "";
+            textBoxSource.TextChanged += testOutput_TextChanged;
+            // 
+            // labelResizeImage
+            // 
+            labelResizeImage.AutoSize = true;
+            labelResizeImage.Location = new Point(112, 85);
+            labelResizeImage.Name = "labelResizeImage";
+            labelResizeImage.Size = new Size(137, 25);
+            labelResizeImage.TabIndex = 3;
+            labelResizeImage.Text = "Selected Folder:";
+            // 
+            // labelOutputLocation
+            // 
+            labelOutputLocation.AutoSize = true;
+            labelOutputLocation.Location = new Point(532, 97);
+            labelOutputLocation.Name = "labelOutputLocation";
+            labelOutputLocation.Size = new Size(177, 25);
+            labelOutputLocation.TabIndex = 6;
+            labelOutputLocation.Text = "Selected Destination:";
+            // 
+            // textBoxDestination
+            // 
+            textBoxDestination.Location = new Point(490, 125);
+            textBoxDestination.Name = "textBoxDestination";
+            textBoxDestination.Size = new Size(252, 178);
+            textBoxDestination.TabIndex = 5;
+            textBoxDestination.Text = "";
+            // 
+            // buttonSelectFolderDistination
+            // 
+            buttonSelectFolderDistination.Location = new Point(553, 318);
+            buttonSelectFolderDistination.Name = "buttonSelectFolderDistination";
+            buttonSelectFolderDistination.Size = new Size(134, 51);
+            buttonSelectFolderDistination.TabIndex = 4;
+            buttonSelectFolderDistination.Text = "Select Folder";
+            buttonSelectFolderDistination.UseVisualStyleBackColor = true;
+            buttonSelectFolderDistination.Click += buttonSelectFolderDistination_Click;
             // 
             // MainWindow
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(testOutput);
-            Controls.Add(test);
+            Controls.Add(labelOutputLocation);
+            Controls.Add(textBoxDestination);
+            Controls.Add(buttonSelectFolderDistination);
+            Controls.Add(labelResizeImage);
+            Controls.Add(textBoxSource);
+            Controls.Add(buttonResizeImages);
             Controls.Add(SelectFolder);
             Name = "MainWindow";
             Text = "Main";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button SelectFolder;
-        private Button test;
-        private RichTextBox testOutput;
+        private Button buttonResizeImages;
+        private RichTextBox textBoxSource;
+        private Label labelResizeImage;
+        private Label labelOutputLocation;
+        private RichTextBox textBoxDestination;
+        private Button buttonSelectFolderDistination;
     }
 }
--- Ironhorse Invoice Assistant C#/MainWindow.cs
+++ Ironhorse Invoice Assistant C#/MainWindow.cs
@@ -17,23 +17,33 @@ namespace Ironhorse_Invoice_Assistant_C_
 
         private void SelectFolder_Click(object sender, EventArgs e)
         {
-            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            FolderBrowserDialog folderBrowserSource = new FolderBrowserDialog();
             {
-                folderBrowserDialog.ShowDialog();
-                selectedpath = folderBrowserDialog.SelectedPath;
-                testOutput.Text = selectedpath;
+                folderBrowserSource.ShowDialog();
+                selectedpath = folderBrowserSource.SelectedPath;
+                textBoxSource.Text = selectedpath;
             }
 
         }
 
         private void test_Click(object sender, EventArgs e)
         {
-    
+
         }
 
         private void testOutput_TextChanged(object sender, EventArgs e)
         {
-            
+
+        }
+
+        private void buttonSelectFolderDistination_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDestination = new FolderBrowserDialog();
+            {
+                folderBrowserDestination.ShowDialog();
+                selectedpath = folderBrowserDestination.SelectedPath;
+                textBoxDestination.Text = selectedpath;
+            }
         }
     }
 }
